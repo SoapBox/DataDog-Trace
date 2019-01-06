42948b819dc035fee3ed353694e1fa8f6027f410
Justin Hayes <justin@soapboxhq.com>
2019-01-06 17:02:34 -0500

    Add docblocks

--- src/Trace.php
+++ src/Trace.php
@@ -7,9 +7,23 @@ use OpenTracing\GlobalTracer;
 
 class Trace
 {
+    /**
+     * @var array
+     */
     private $tags = [];
+
+    /**
+     * @var array
+     */
     private $tagCallbacks = [];
 
+    /**
+     * Create a new trace for the given class and method
+     *
+     * @param string $class
+     * @param string $method
+     * @param bool $static
+     */
     public function __construct(string $class, string $method, bool $static = false)
     {
         $this->class = $class;
@@ -20,6 +34,9 @@ class Trace
         $this->asWebType();
     }
 
+    /**
+     * Register this trace with the datadog tracer
+     */
     public function __destruct()
     {
         $class = $this->class;
@@ -56,42 +73,83 @@ class Trace
         });
     }
 
+    /**
+     * Set the resource tag for this trace
+     *
+     * @param string $resource
+     *
+     * @return self
+     */
     public function setResource(string $resource)
     {
         $this->withTags([\DDTrace\Tags\RESOURCE_NAME => $resource]);
         return $this;
     }
 
+    /**
+     * Set the service name tag for this trace
+     *
+     * @param string $name
+     *
+     * @return self
+     */
     public function setServiceName(string $name)
     {
         $this->withTags([\DDTrace\Tags\SERVICE_NAME => $name]);
         return $this;
     }
 
+    /**
+     * Mark the span type as cache for this trace
+     *
+     * @return self
+     */
     public function asCacheType()
     {
         $this->withTags([\DDTrace\Tags\SPAN_TYPE => \DDTrace\Types\CACHE]);
         return $this;
     }
 
+    /**
+     * Mark the span type as http client for this trace
+     *
+     * @return self
+     */
     public function asHttpType()
     {
         $this->withTags([\DDTrace\Tags\SPAN_TYPE => \DDTrace\Types\HTTP_CLIENT]);
         return $this;
     }
 
+    /**
+     * Mark the span type as web for this trace
+     *
+     * @return self
+     */
     public function asWebType()
     {
         $this->withTags([\DDTrace\Tags\SPAN_TYPE => \DDTrace\Types\WEB_SERVLET]);
         return $this;
     }
 
+    /**
+     * Mark the span type as sql for this trace
+     *
+     * @return self
+     */
     public function asSqlType()
     {
         $this->withTags([\DDTrace\Tags\SPAN_TYPE => \DDTrace\Types\SQL]);
         return $this;
     }
 
+    /**
+     * Add th given tags to this trace
+     *
+     * @param array|callable $tags
+     *
+     * @return self
+     */
     public function withTags($tags)
     {
         if (is_callable($tags)) {
